d2cf4edcda094ffcf9c9257dc7be95414e3ee5b9
Fernando <fernandomffmmffm@gmail.com>
2026-02-27 22:28:11 -0300

    Mais 1 mod

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ function abrirAba(aba) {
 
 
 async function transferir() {
-  const origemId = parseInt(document.getElementById('origem').value);A
+  const origemId = parseInt(document.getElementById('origem').value);
   const destinoId = parseInt(document.getElementById('destino').value);
   const valor = parseFloat(document.getElementById('valor').value);
 
